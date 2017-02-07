469fbefd20ffbfca20d875cc5a0be859e0e022b5
KowalczykBartek <bartlomiej.kowalczyk@sap.com>
2017-02-07 23:19:01 +0100

    Code style

--- src/com.primary/concurrency/ConcurrencyManager.java
+++ src/com.primary/concurrency/ConcurrencyManager.java
@@ -3,10 +3,9 @@ package com.primary.concurrency;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.EventExecutorGroup;
 
-public class ConcurrencyManager
-{
-	/*
-	 * Cassandra's stage approach - replace later with real manager.
-	 */
-	public static final EventExecutorGroup HTTP_OPERATION_STAGE = new DefaultEventExecutorGroup(15);
+public class ConcurrencyManager {
+    /*
+     * Cassandra's stage approach - replace later with real manager.
+     */
+    public static final EventExecutorGroup HTTP_OPERATION_STAGE = new DefaultEventExecutorGroup(15);
 }
